8a81871566562212af015276af77f4acc46ee633
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2019-05-31 11:42:08 -0300

    Fix bug

--- controllers/usuario/create.js
+++ controllers/usuario/create.js
@@ -3,8 +3,8 @@ const db = require('../../config/database');
 module.exports = async (req, res) => {
     try {
         db.insert(req.body, (err, response) => {
-            if (err.errorType === 'uniqueViolated') res.location(`${req.originalUrl}/${req.body._id}`).status(303).json();
-            else if (response.length) res.status(201).json({ error: 0, payload: { comments: response } });
+            if (err && err.errorType === 'uniqueViolated') res.location(`${req.originalUrl}/${req.body._id}`).status(303).json();
+            else if (response) res.status(201).json({ error: 0, payload: { comments: response } });
             else res.status(500).json({ error: { message: err.message }, payload: null });
         });
     } catch (err) {
